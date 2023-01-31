b2a70c6832283e6103d1bac686513b24f7533dc3
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-31 23:09:59 +0600

    index: Added locationsRouter

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import categoriesRouter from "./routers/categories";
+import locationsRouter from "./routers/locations";
 
 const app = express();
 const port = 8000;
@@ -8,6 +9,7 @@ const port = 8000;
 app.use(cors());
 app.use(express.static("public"));
 app.use('/categories', categoriesRouter);
+app.use('/locations', locationsRouter);
 app.use(express.json());
 
 app.listen(port, () => {
