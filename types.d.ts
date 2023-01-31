dc09607dfc55948c1ad52997bd748455f2ebc602
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-31 22:51:44 +0600

    types.d.ts: Created types Category, Location, Record

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,20 @@
-type Image = string | null;
+export interface Category {
+    id: number;
+    title: string;
+    description: string;
+}
+
+export interface Location {
+    id: number;
+    title: string;
+    description: string;
+}
+
+export interface Record {
+    id: number;
+    category_id: number;
+    location_id: number;
+    title: string;
+    description: string;
+    image: string | null;
+}
