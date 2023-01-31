5f7b20458f700ebcc4b58cae9e4272b963b826f0
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-31 23:36:09 +0600

    api: Created inventory.sql Added sql requests to create inventory schema and its tables

---
+++ inventory.sql
@@ -0,0 +1,35 @@
+create schema inventory collate utf8mb3_general_ci;
+
+create table categories
+(
+    id          int auto_increment,
+    title       varchar(200) not null,
+    description text         not null,
+    constraint categories_pk
+            primary key (id)
+);
+
+create table locations
+(
+    id          int auto_increment,
+    title       varchar(200) not null,
+    description text         not null,
+    constraint locations_pk
+            primary key (id)
+);
+
+create table records
+(
+    id          int auto_increment,
+    category_id int          not null,
+    location_id int          not null,
+    title       varchar(200) not null,
+    description text         not null,
+    image       varchar(100) null,
+    constraint records_pk
+        primary key (id),
+    constraint records_categories_id_fk
+        foreign key (category_id) references categories (id),
+    constraint records_locations_id_fk
+        foreign key (location_id) references locations (id)
+);
