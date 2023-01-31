5c283ffd16b9790a935487ba422ba2042c64f7bb
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-31 23:04:19 +0600

    index: Added categoriesRoute

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express from "express";
+import categoriesRouter from "./routers/categories";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.static("public"));
+app.use('/categories', categoriesRouter);
 app.use(express.json());
 
 app.listen(port, () => {
