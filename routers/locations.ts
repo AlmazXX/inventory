bf0387ff13be19bfeb7fb96ff89e57f03e7dd083
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-01 21:05:58 +0600

    locations.ts: Added requestsProcessing with mysql

--- routers/locations.ts
+++ routers/locations.ts
@@ -1,21 +1,49 @@
 import {Router} from "express";
+import mysqlDb from "../mysqlDb";
+import {Category, Location} from "../types";
+import {OkPacket} from "mysql2";
 
 const locationsRouter = Router();
 
-locationsRouter.get('/', (req, res) => {
-    res.send('Locations will be here');
+locationsRouter.get('/', async (req, res) => {
+    const connection = mysqlDb.getConnection();
+    const query = await connection.query('SELECT * FROM locations');
+    const locations = query[0] as Location[]
+    res.send(locations);
 });
 
-locationsRouter.get('/:id', (req, res) => {
-    res.send('A location will be here');
+locationsRouter.get('/:id', async (req, res) => {
+    const connection = mysqlDb.getConnection();
+    const query = await connection.query('SELECT * FROM locations WHERE id = ?', [req.params.id]);
+    const locations = query[0] as Category[]
+    const location = locations[0]
+    if (!location) {
+        return res.status(404).send({error: 'Not found'})
+    }
+    res.send(location);
 });
 
-locationsRouter.post('/', (req, res) => {
-    res.send('A location will be created here');
+locationsRouter.post('/', async (req, res) => {
+    if (!req.body.title) {
+        return res.status(404).send({error: 'Field "Title" is required'})
+    }
+
+    const locationData = {
+        title: req.body.title,
+        description: req.body.description,
+    }
+
+    const connection = mysqlDb.getConnection();
+    const query = await connection.query('INSERT INTO locations (title, description) VALUES (?, ?)', [locationData.title, locationData.description]);
+    const info = query[0] as OkPacket;
+
+    res.send({...locationData, id: info.insertId});
 });
 
-locationsRouter.delete('/:id', (req, res) => {
-    res.send('A location will be deleted here');
+locationsRouter.delete('/:id', async (req, res) => {
+    const connection = mysqlDb.getConnection();
+    await connection.query('DELETE FROM locations WHERE id = ?', [req.params.id])
+    res.send('The location is deleted');
 });
 
 export default locationsRouter;
